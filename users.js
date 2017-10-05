9a64aca4e3527116423f3e94a3112d20a8f34470
rebbapr2 <32557054+rebbapr2@users.noreply.github.com>
2017-10-05 18:05:13 -0500

    Add files via upload

---
+++ users.js
@@ -0,0 +1,49 @@
+(function (){
+    
+    'use strict';
+
+var express = require('express'),
+  router = express.Router(),
+  logger = require('../../config/logger');
+
+module.exports = function (app, config) {
+	app.use('/api', router);
+    router.get('/users', function(req, res,next){
+        logger.log('Get all users','verbose');
+        res.status(200).json({message:'Get All Users'});
+    });
+    router.get('/user/:userId', function(req, res, next){
+        logger.log('Get user' + req.params.id, 'verbose');
+    
+        res.status(200).json({message:'Get User' + req.params.id}); 
+    });
+
+    router.post('/users',function(req,res,next){
+        logger.log('Create User', 'verbose');
+        res.status(201).json({message:'Create User' + req.params.id});
+    });
+
+    router.put('/user/:userId', function(req, res, next){
+        logger.log('Update user' + req.params.id, 'verbose');
+    
+        res.status(200).json({message:'Update User' + req.params.id}); 
+    });
+
+    router.delete('/user/:userId', function(req, res, next){
+        logger.log('Delete user' + req.params.id, 'verbose');
+    
+        res.status(200).json({message:'Delete User' + req.params.id}); 
+
+        router.post('/login', function(req, res, next){
+            console.log(req.body);
+            var email = req.body.email;
+            var password = req.body.password;
+      
+            var obj = {'email' : email, 'password' : password};
+          res.status(201).json(obj);
+      });
+      
+    });
+
+};
+})();
