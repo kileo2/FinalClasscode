9d3b354ccc0e8d5a30bcaaf554cfef74f7b49b01
sni3479 <alexsniffin@northwesternmutual.com>
2017-10-05 19:25:10 -0500

    Format

--- config/express.js
+++ config/express.js
@@ -1,69 +1,68 @@
 var express = require('express');
 var logger = require('./logger');
-var morgan =require('morgan');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
 
 module.exports = function (app, config) {
 
-  if(process.env.NODE_ENV !== 'test') {
-    app.use(morgan('dev'));
+    if (process.env.NODE_ENV !== 'test') {
+        app.use(morgan('dev'));
 
-    app.use(function (req, res, next) {
-      logger.log('Request from ' + req.connection.remoteAddress, 'info');
-      next();
+        app.use(function (req, res, next) {
+            logger.log('Request from ' + req.connection.remoteAddress, 'info');
+            next();
+        });
+    }
+
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    var users = [{name: 'John', email: 'woo@hoo.com'},
+        {name: 'Betty', email: 'loo@woo.com'},
+        {name: 'Hal', email: 'boo@woo.com'}
+    ];
+
+    app.get('/api/users', function (req, res) {
+        res.status(200).json(users);
     });
-  }
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+    function One(req, res, next) {
+        res.set('X-One', 'One');
+        next();
+    }
 
-  var users = [	{name: 'John', email: 'woo@hoo.com'},
-  {name: 'Betty', email: 'loo@woo.com'},
-  {name: 'Hal', email: 'boo@woo.com'}
-];
+    function Two(req, res, next) {
+        res.set('X-Two', 'Two');
+        next();
+    }
 
-app.get('/api/users', function (req, res) {
-  res.status(200).json(users);
-});
- 
+    app.get('/', [One, Two], function (req, res) {
+        res.send('Three');
+    });
 
+    require('../app/controllers/users')(app, config);
 
-function One(req, res, next){
-	res.set('X-One','One');
-	next();
-}
+    app.use(express.static(config.root + '/public'));
 
-function Two(req, res, next){
-	res.set('X-Two','Two');
-	next();
-}
+    app.use(function (req, res) {
+        res.type('text/plan');
+        res.status(404);
+        res.send('404 Not Found');
+    });
 
-app.get('/', [One, Two], function(req, res){
-	res.send('Three');
-});
+    app.use(function (err, req, res, next) {
+        console.error(err.stack);
+        res.type('text/plan');
+        res.status(500);
+        res.send('500 Sever Error');
+    });
 
-require('../app/controllers/users')(app,config);
-
-app.use(express.static(config.root + '/public'));
-
-  app.use(function (req, res) {
-    res.type('text/plan');
-    res.status(404);
-    res.send('404 Not Found');
-  });
-
-  app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.type('text/plan');
-    res.status(500);
-    res.send('500 Sever Error');
-  });
-
-  logger.log("Starting application");
+    logger.log("Starting application");
 
 };
 
--- index.js
+++ index.js
@@ -9,46 +9,46 @@ var app = express();
 
 require('./config/express')(app, config);
 
-logger.log('info',"Creating HTTP server on port: %s", config.port);
+logger.log('info', "Creating HTTP server on port: %s", config.port);
 require('http').createServer(app).listen(config.port, function () {
-    logger.log("HTTP Server listening on port:" + " " +  config.port + " "+ app.get('env')+ "localhost:" + config.port);
+    logger.log("HTTP Server listening on port:" + " " + config.port + " " + app.get('env') + "localhost:" + config.port);
 });
 
 module.exports = app;
 
 
 /*
-ss.static(path.normalize(__dirname) + '/public'));
+ ss.static(path.normalize(__dirname) + '/public'));
 
 
-app.use(function(req, res, next){
-    console.log('Request from ' + req.ip);
-    next();
-  });  
-  
-app.get('/', function(req,res){
-	res.send('Hello World!');
-});
+ app.use(function(req, res, next){
+ console.log('Request from ' + req.ip);
+ next();
+ });
 
-app.get('/about', function(req, res){
-	res.send('About Us!');
-});
+ app.get('/', function(req,res){
+ res.send('Hello World!');
+ });
+
+ app.get('/about', function(req, res){
+ res.send('About Us!');
+ });
 
 
-app.use(function(req, res){
-	res.type('text/plan');
-	res.status(404);
-	res.send('404 Not Found');
-});
+ app.use(function(req, res){
+ res.type('text/plan');
+ res.status(404);
+ res.send('404 Not Found');
+ });
 
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.type('text/plan');
-	res.status(500);
-	res.send('500 Sever Error');
-});
+ app.use(function(err, req, res, next){
+ console.error(err.stack);
+ res.type('text/plan');
+ res.status(500);
+ res.send('500 Sever Error');
+ });
 
-app.listen(app.get('port'), function(){
-	console.log('Express started on http://localhost:' + app.get('port'));
-});
-*/
+ app.listen(app.get('port'), function(){
+ console.log('Express started on http://localhost:' + app.get('port'));
+ });
+ */
